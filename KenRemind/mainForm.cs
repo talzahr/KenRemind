02effa29fa3b3fa1b0de86ca503b2ac859a7f253
Talzahr <krozareq@gmail.com>
2024-04-22 16:28:48 -0500

    Fix loading from XML into 24H format

--- KenRemind/mainForm.cs
+++ KenRemind/mainForm.cs
@@ -35,14 +35,33 @@ namespace KenRemind
             dataGridView_Reminders.ScrollBars = ScrollBars.Both;
             comboBox_Period.SelectedIndex = 0;
 
+            dataGridView_Reminders.CellFormatting += new DataGridViewCellFormattingEventHandler(dataGridView_Reminders_CellFormatting);
+
             this.Load += new EventHandler(mainForm_Load);
             this.FormClosing += new FormClosingEventHandler(mainForm_FormClosing);
+        }
+
+        void dataGridView_Reminders_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView_Reminders.Columns["Date & Time"].Index && e.Value != null)
+            {
+                DateTime dateTime = DateTime.Parse(e.Value.ToString());
+                e.Value = dateTime.ToString("MM/dd/yyyy hh:mm tt");
+                e.FormattingApplied = true;
+            }
         }
 
         private void button_AddTask_Click(object sender, EventArgs e)
         {
             int hour = Convert.ToInt32(numericUpDown_Hour.Value);
             int minute = Convert.ToInt32(numericUpDown_Min.Value);
+            string period = comboBox_Period.SelectedItem.ToString();
+
+            // Adjust hour based on the period
+            if (period == "PM" && hour != 12)
+                hour += 12;
+            else if (period == "AM" && hour == 12)
+                hour = 0;
 
             DateTime date = dateTimePicker.Value.Date;
             DateTime dateTime = new DateTime(date.Year, date.Month, date.Day, hour, minute, 0);
